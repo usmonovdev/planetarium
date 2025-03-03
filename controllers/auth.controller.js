99f19848b46fc8f55b88feb6b8c8aca2fadcaf0c
azizziy <usmonovazizbek045@gmail.com>
2025-03-03 21:45:54 +0500

    Update password. Update balance. Activate profile

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -95,3 +95,83 @@ exports.updateProfile = AsyncHandler(async (req, res, next) => {
     data: userUpdated,
   });
 });
+
+// @desc        Update password
+// @route       PUT /api/v1/auth/profile/update/password
+// @access      Private
+exports.updatePassword = AsyncHandler(async (req, res, next) => {
+  // Find user
+  const user = await User.findById(req.user._id);
+  const { oldPassword, newPassword } = req.body;
+
+  // Check old password
+  if (!(await user.matchPassword(oldPassword))) {
+    return next(new ErrorResponse("Old password is incorrect", 400));
+  }
+
+  // Update password and save
+  user.password = newPassword;
+  await user.save();
+
+  // Generate new token for user
+  const token = user.generateJwtToken();
+
+  res.status(200).json({
+    success: true,
+    message: "Password updated and new JwtToken generated",
+    data: user,
+    token,
+  });
+});
+
+// @desc        Payment Balance
+// @route       POST /api/v1/auth/profile/payment/balance
+// @access      Private
+exports.paymentBalance = AsyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user._id)
+
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user._id,
+    { balance: (user.balance + req.body.payment) },
+    {
+      new: true,
+    }
+  );
+
+  res.status(200).json({
+    success: true,
+    data: updatedUser,
+  });
+});
+
+// @desc        Activate profile
+// @route       GET /api/v1/auth/profile/activate
+// @access      Private
+exports.activateProfile = AsyncHandler(async (req, res, next) => {
+  const apiCost = process.env.API_COST;
+
+  const user = await User.findById(req.user._id);
+
+  if (user.balance < apiCost) {
+    const needMore = apiCost - user.balance;
+    return next(
+      new ErrorResponse(
+        `Your balance less than ${apiCost}. You need ${needMore}`
+      )
+    );
+  }
+
+  await User.findByIdAndUpdate(
+    req.user._id,
+    { balance: user.balance - apiCost, isActive: true },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).json({
+    success: true,
+    message: "Your profile activated",
+  });
+});
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,6 +5,9 @@ const {
   login,
   getProfile,
   updateProfile,
+  updatePassword,
+  paymentBalance,
+  activateProfile,
 } = require("../controllers/auth.controller");
 const { protected } = require("../middleware/auth");
 
@@ -12,5 +15,8 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/profile", protected, getProfile);
 router.put("/profile/update", protected, updateProfile);
+router.put("/profile/update/password", protected, updatePassword);
+router.post("/profile/payment/balance", protected, paymentBalance);
+router.get("/profile/activate", protected, activateProfile);
 
 module.exports = router;
